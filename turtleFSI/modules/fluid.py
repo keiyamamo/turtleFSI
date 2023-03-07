40ced609e6ee4a0f15122b1dfe01ad8ac26a627d
Kei <withwhom0215@gmail.com>
2023-03-07 21:23:18 +0100

    checking fluid formulation

--- turtleFSI/modules/fluid.py
+++ turtleFSI/modules/fluid.py
@@ -34,26 +34,37 @@ def fluid_setup(v_, p_, d_, psi, gamma, dx_f, dx_f_id_list, fluid_properties, k,
         F_fluid_linear += rho_f / k * inner(J_(d_["n-1"]) * theta1 * (v_["n"] - v_["n-1"]), psi) * dx_f[fluid_region]
     
         # Convection
+        # TODO: need to check
         F_fluid_nonlinear += theta0 * rho_f * inner(grad(v_["n"]) * inv(F_(d_["n"])) *
                                                     J_(d_["n"]) * v_["n"], psi) * dx_f[fluid_region]
         F_fluid_linear += theta1 * rho_f * inner(grad(v_["n-1"]) * inv(F_(d_["n-1"])) *
                                                  J_(d_["n-1"]) * v_["n-1"], psi) * dx_f[fluid_region]
     
         # Stress from pressure
+        # NOTE: seems correct. The first term of hat(A)_P(hat(U))(hat(Psi)) in eq (5.4) p57 in WickPhD
         F_fluid_nonlinear += inner(J_(d_["n"]) * sigma_f_p(p_["n"], d_["n"]) *
                                    inv(F_(d_["n"])).T, grad(psi)) * dx_f[fluid_region]
     
         # Stress from velocity
+        # TODO: need to check
         F_fluid_nonlinear += theta0 * inner(J_(d_["n"]) * sigma_f_u(v_["n"], d_["n"], mu_f) *
                                             inv(F_(d_["n"])).T, grad(psi)) * dx_f[fluid_region]
         F_fluid_linear += theta1 * inner(J_(d_["n-1"]) * sigma_f_u(v_["n-1"], d_["n-1"], mu_f)
                                          * inv(F_(d_["n-1"])).T, grad(psi)) * dx_f[fluid_region]
     
         # Divergence free term
+        # NOTE: seems correct. The second term of hat(A)_I(hat(U))(hat(Psi)) in eq (5.4) p57 in WickPhD
         F_fluid_nonlinear += inner(div(J_(d_["n"]) * inv(F_(d_["n"])) * v_["n"]), gamma) * dx_f[fluid_region]
     
         # ALE term
-        F_fluid_nonlinear -= rho_f / k * inner(J_(d_["n"]) * grad(v_["n"]) * inv(F_(d_["n"])) *
-                                           (d_["n"] - d_["n-1"]), psi) * dx_f[fluid_region]
+        # NOTE: The original formulation is:
+        # F_fluid_nonlinear -= rho_f / k * inner(J_(d_["n"]) * grad(v_["n"]) * inv(F_(d_["n"])) *
+        #                                    (d_["n"] - d_["n-1"]), psi) * dx_f[fluid_region]
+        # NOTE: However, this formualation seems incorrect, as it misinterprets the grad and nabla_grad operators. 
+        #       grad should be used for the J * inv(F) * (d_["n"] - d_["n-1"] and not the v_["n"].
+
+        # NOTE: The correct formulation is (?):
+        F_fluid_nonlinear -= rho_f / k * inner(grad(J_(d_["n"]) * inv(F_(d_["n"])) *
+                                           (d_["n"] - d_["n-1"])) * v_["n"], psi) * dx_f[fluid_region]
 
     return dict(F_fluid_linear=F_fluid_linear, F_fluid_nonlinear=F_fluid_nonlinear)
