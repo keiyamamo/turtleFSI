7c9c2c6f8ab2855dadd2329a56ea3a6cfe8e8d42
Kei <withwhom0215@gmail.com>
2023-03-09 17:51:17 +0100

    tg2d works with unstructured mesh

--- turtleFSI/problems/tg2d.py
+++ turtleFSI/problems/tg2d.py
@@ -1,20 +1,28 @@
-from turtleFSI.problems import *
+import matplotlib.pyplot as plt
+import pickle
+from os import path
+import sys
+try:
+    import pygmsh
+except ImportError:
+    pass
+
 from dolfin import *
+from turtleFSI.problems import *
 
 """
-Note: Pressure is far from correct, but the velocity beahves nicely. Pressure gets wrong inside newton loop.
-Date: 2022-10-10
+Taylor-Green vortex in 2D with fixed domain size
+This problem can be used to test the accuracy of the fluid solver
 """
 
 # Override some problem specific parameters
 def set_problem_parameters(default_variables, **namespace):
     default_variables.update(dict(
         mu_f=0.01, # dynamic viscosity of fluid, 0.01 as kinematic viscosity
-        T=6,
-        dt=0.03125,
-        theta=0.5,
-        rho_f = 1,
-        Nx=100, Ny=100,
+        T=0.01,
+        dt=0.1,
+        theta=0.5, # Crank-Nicolson
+        rho_f = 1, # density of fluid
         folder="tg2d_results",
         solid = "no_solid",
         extrapolation="no_extrapolation", # first try with static mesh
@@ -23,11 +31,14 @@ def set_problem_parameters(default_variables, **namespace):
         checkpoint_step=100,
         compute_error=100,
         L = 2.,
-        v_deg=4,
-        d_deg=4,
-        p_deg=3,
+        v_deg=2,
+        p_deg=1,
+        atol=1e-10,
+        rtol=1e-10,
         total_error_v = 0,
-        total_error_p = 0
+        total_error_p = 0,
+        mesh_size=0.2,
+        mesh_type="unstructured",
         ))
 
     return default_variables
@@ -36,8 +47,43 @@ class Wall(SubDomain):
         def inside(self, x, on_boundary):
             return on_boundary
 
-def get_mesh_domain_and_boundaries(L, Nx, Ny, **namespace):
-    mesh = RectangleMesh(Point(-L / 2, -L / 2), Point(L / 2, L / 2), Nx, Ny)
+def create2Dmesh(msh):
+    # remove z coordinate
+    msh.points = msh.points[:, :2]
+    nodes = msh.points
+    cells = msh.cells_dict["triangle"].astype(np.uintp)
+    mesh = Mesh()
+    editor = MeshEditor()
+    # point, interval, triangle, quadrilateral, hexahedron
+    editor.open(mesh, "triangle", 2, 2)
+    editor.init_vertices(len(nodes))
+    editor.init_cells(len(cells))
+    [editor.add_vertex(i, n) for i, n in enumerate(nodes)]
+    [editor.add_cell(i, n) for i, n in enumerate(cells)]
+    editor.close()
+    return mesh
+
+def fac_mesh(mesh_size):
+    with pygmsh.geo.Geometry() as geom:
+        geom.add_rectangle(
+            -1, 1, -1, 1, 0.0, mesh_size=mesh_size
+        )
+        mesh = geom.generate_mesh()
+        mesh = create2Dmesh(mesh)
+    return mesh
+
+def get_mesh_domain_and_boundaries(mesh_size, mesh_type, **namespace):
+    """
+    Here, we create unstructured mesh using gmsh. 
+    If pygmsh is not installed, we use default mesh from dolfin, which is structured.
+    args:
+        mesh_size: scaling factor for mesh size in gmsh. The lower the value, the finer the mesh.
+    """
+    if "pygmsh" in sys.modules and mesh_type == "unstructured":
+        mesh = fac_mesh(mesh_size)
+    else:
+        mesh = RectangleMesh(Point(-1, -1), Point(1, 1), 10, 10)
+      
     # Mark the boundaries
     Allboundaries = DomainBoundary()
     boundaries = MeshFunction("size_t", mesh, mesh.geometry().dim() - 1)
@@ -146,7 +192,13 @@ def post_solve(DVP, dt, dvp_, total_error_v, total_error_p, velocity, p_bc_val, 
   
     return dict(total_error_v=total_error_v, total_error_p=total_error_p)                 
       
-def finished(total_error_v, total_error_p, **namespace):
+def finished(total_error_v, total_error_p, mesh_size, dt, results_folder, **namespace):
+
     if MPI.rank(MPI.comm_world) == 0:
         print("total error for the velocity: ", total_error_v)
         print("total error for the pressure: ", total_error_p)
+
+    save_data = dict(total_error_v=total_error_v, total_error_p=total_error_p, mesh_size=mesh_size, dt=dt)
+    file_name = f'mesh_size_{mesh_size}_dt_{dt}.pickle'
+    with open(path.join(results_folder, file_name), 'wb') as f:
+        pickle.dump(save_data, f)
