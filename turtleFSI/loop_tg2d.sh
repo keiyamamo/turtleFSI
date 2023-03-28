dced2753362128849be62281468432ae15688221
Kei <withwhom0215@gmail.com>
2023-03-28 12:43:09 +0200

    update bash script

--- turtleFSI/loop_tg2d.sh
+++ turtleFSI/loop_tg2d.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-dt=0.001
+dt=0.0001
 v_deg=2
 p_deg=1
-T=1.6
+T=1
 # first we loop over spatial resolution
-for i in {2..3}
+for i in {1..6}
 do  
     Nx=$((10*2**$i))
-    mesh_size=$(echo "scale=6; 1.0/$Nx" | bc)
+    mesh_size=$(echo "scale=6; 4.0/$Nx" | bc)
     # swith the number of processors
-    if [ $i -eq 4 ]; then
+    if [ $i -eq 4 ] || [ $i -eq 5 ] || [ $i -eq 6 ] ; then
         mpirun -np 4 python -u monolithic.py -p=tg2d -dt=$dt -T=$T --new-arguments mesh_size=${mesh_size} folder=T_${T}_mesh_size_${mesh_size}_dt_${dt}_P${v_deg}P${p_deg}_tg2d
     else
-        mpirun -np 4 python -u monolithic.py -p=tg2d -dt=$dt -T=$T --new-arguments mesh_size=${mesh_size} folder=T_${T}_mesh_size_${mesh_size}_dt_${dt}_P${v_deg}P${p_deg}_tg2d
+        mpirun -np 2 python -u monolithic.py -p=tg2d -dt=$dt -T=$T --new-arguments mesh_size=${mesh_size} folder=T_${T}_mesh_size_${mesh_size}_dt_${dt}_P${v_deg}P${p_deg}_tg2d
     fi
 done
 
