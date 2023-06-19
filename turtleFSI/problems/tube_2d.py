d35d4f6b7592839471e683d84b51c72e6ad862ce
Kei <withwhom0215@gmail.com>
2023-06-19 15:27:12 +0200

    checking if pressure_bc works

--- turtleFSI/problems/tube_2d.py
+++ turtleFSI/problems/tube_2d.py
@@ -10,21 +10,21 @@ Last update: 2023--6-19
 def set_problem_parameters(default_variables, **namespace):
 
     # Overwrite default values
-    E_s_val = 1E7    # Young modulus (elasticity) [dyn/cm2]
-    nu_s_val = 0.3   # Poisson ratio (compressibility)
+    E_s_val = 1E6    # Young modulus (elasticity) [dyn/cm2]
+    nu_s_val = 0.45   # Poisson ratio (compressibility)
     mu_s_val = E_s_val/(2*(1+nu_s_val))
     lambda_s_val = nu_s_val*2.*mu_s_val/(1. - 2.*nu_s_val)
 
     default_variables.update(dict(
-        T=2e-5,                 # Simulation end time [s]
-        dt=2e-7,                # Time step size [s]
+        T=0.1,                 # Simulation end time [s]
+        dt=0.0005,                # Time step size [s]
         recompute=30,           # Recompute the Jacobian matix within time steps                                                                                    
         recompute_tstep=10,     # Recompute the Jacobian matix over time steps (dangerous!)  
         folder="tube_2d",       # Output folder
-        theta=0.50+2e-7,        # Shifted Crank-Nicolson scheme
-        rho_f=1.0,              # Fluid density [g/cm3]
-        mu_f=0.04,              # Fluid dynamic viscosity [poise]
-        rho_s=1.0,              # Solid density [g/cm3]
+        theta=0.50+0.0005,        # Shifted Crank-Nicolson scheme
+        rho_f=025E3,              # Fluid density [g/cm3]
+        mu_f=3.5E-3,              # Fluid dynamic viscosity [poise]
+        rho_s=1.0E3,              # Solid density [g/cm3]
         mu_s=mu_s_val,          # Solid shear modulus or 2nd Lame Coef. 
         nu_s=nu_s_val,          # Solid Poisson ratio [-]
         lambda_s=lambda_s_val,  # Solid Young's modulus 
@@ -32,7 +32,10 @@ def set_problem_parameters(default_variables, **namespace):
         dx_s_id=8,              # ID of marker in the solid domain
         checkpoint_step=50,     # Save frequency of checkpoint files 
         save_step=1,            # Save frequency of files for visualisation
-        save_deg=2,
+        save_deg=1,
+        d_deg = 2,
+        v_deg = 2,
+        P_deg = 1,
         t_start = 2e-6,         # Start time of the impulse
         t_end = 2.1e-6,           # End time of the impulse
     ))
@@ -89,20 +92,21 @@ class PressureImpulse(UserExpression):
     def value_shape(self):
         return (2,)
     
-def initiate(dvp_, DVP, mesh, boundaries, **namespace):
-    # get the dofs on the fluid inlet boundary
-    V = FunctionSpace(mesh, 'CG', 1)
-    bc = DirichletBC(V, Constant(0), boundaries, 1)
-    bdry_dofs = np.array(list(bc.get_boundary_values().keys())) 
-    p_n = dvp_["n"].sub(2, deepcopy=True) 
-    p_n.vector()[bdry_dofs] = 5e3
-    bc = DirichletBC(DVP.sub(2), p_n, boundaries, 1)
+# def initiate(dvp_, DVP, mesh, boundaries, **namespace):
+#     # get the dofs on the fluid inlet boundary
+#     # V = FunctionSpace(mesh, 'CG', 1)
+#     # bc = DirichletBC(V, Constant(0), boundaries, 1)
+#     # bdry_dofs = np.array(list(bc.get_boundary_values().keys())) 
+#     # p_n = dvp_["n"].sub(2, deepcopy=True) 
+#     # p_n.vector()[bdry_dofs] = 5e3
+#     # bc = DirichletBC(DVP.sub(2), p_n, boundaries, 1)
     
-    bc.apply(dvp_["n"].vector())
+#     # bc.apply(dvp_["n"].vector())
 
-    return dict(dvp_=dvp_)
+#     # return dict(dvp_=dvp_)
+#     pass
 
-def create_bcs(DVP, boundaries, **namespace):
+def create_bcs(DVP, boundaries, dvp_, mesh, **namespace):
 
     if MPI.rank(MPI.comm_world) == 0:
         print("Create bcs")
@@ -117,8 +121,15 @@ def create_bcs(DVP, boundaries, **namespace):
 
     # Fluid velocity BCs / no-slip at solid walls
     u_f_walls = DirichletBC(DVP.sub(1), ((0.0, 0.0)), boundaries, 3)
+
+    V = FunctionSpace(mesh, 'CG', 1)
+    bc = DirichletBC(V, Constant(0), boundaries, 1)
+    bdry_dofs = np.array(list(bc.get_boundary_values().keys())) 
+    p_n = dvp_["n"].sub(2, deepcopy=True) 
+    p_n.vector()[bdry_dofs] = -5e3
+    bcp = DirichletBC(DVP.sub(2), p_n, boundaries, 1)
     
     # Assemble boundary conditions
-    bcs = [d_s_inlet, d_s_outlet, d_f_inlet, d_f_outlet, u_f_walls]
+    bcs = [d_s_inlet, d_s_outlet, d_f_inlet, d_f_outlet, u_f_walls, bcp]
 
     return dict(bcs=bcs)
