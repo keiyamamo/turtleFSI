b30824ae510d3a956f30d77e21dfb9355c474227
Kei <withwhom0215@gmail.com>
2023-06-22 21:47:51 +0200

    tried to use parabolic pressure profile: solver does not diverge but there is some adverse pressure at the outlet, displacement does not look good

--- turtleFSI/problems/tube_2d.py
+++ turtleFSI/problems/tube_2d.py
@@ -3,7 +3,7 @@ from dolfin import *
 from turtleFSI.problems import *
 
 """
-Last update: 2023--6-19
+Last update: 2023--6-22
 2D tube case intended to simulate propagation of a pressure wave in a tube
 """
 
@@ -16,12 +16,12 @@ def set_problem_parameters(default_variables, **namespace):
     lambda_s_val = nu_s_val*2.*mu_s_val/(1. - 2.*nu_s_val)
 
     default_variables.update(dict(
-        T=0.1,                 # Simulation end time [s]
-        dt=0.0005,                # Time step size [s]
-        recompute=30,           # Recompute the Jacobian matix within time steps                                                                                    
-        recompute_tstep=10,     # Recompute the Jacobian matix over time steps (dangerous!)  
+        T=2,                 # Simulation end time [s]
+        dt=0.001,                # Time step size [s]
+        recompute=5,           # Recompute the Jacobian matix within time steps                                                                                    
+        recompute_tstep=50,     # Recompute the Jacobian matix over time steps (dangerous!)  
         folder="tube_2d",       # Output folder
-        theta=0.50+0.0005,        # Shifted Crank-Nicolson scheme
+        theta=0.501,        # Shifted Crank-Nicolson scheme
         rho_f=025E3,              # Fluid density [g/cm3]
         mu_f=3.5E-3,              # Fluid dynamic viscosity [poise]
         rho_s=1.0E3,              # Solid density [g/cm3]
@@ -49,10 +49,10 @@ def get_mesh_domain_and_boundaries(**namespace):
     with XDMFFile("mesh/tube_2d/tube_2d.xdmf") as infile:
         infile.read(mesh)
     #  # Rescale the mesh coordinated from [mm] to [m]
-    # x = mesh.coordinates()
-    # scaling_factor = 0.001  # from mm to m
-    # x[:, :] *= scaling_factor
-    # mesh.bounding_box_tree().build(mesh)
+    x = mesh.coordinates()
+    scaling_factor = 0.001  # from mm to m
+    x[:, :] *= scaling_factor
+    mesh.bounding_box_tree().build(mesh)
     # Import mesh boundaries
     boundaries = MeshValueCollection("size_t", mesh, 1) 
     with XDMFFile("mesh/tube_2d/tube_2d_facet.xdmf") as infile:
@@ -72,29 +72,104 @@ def get_mesh_domain_and_boundaries(**namespace):
     return mesh, domains, boundaries
 
 
-class PressureImpulse(UserExpression):
-    def __init__(self, force_val, t_start,t_end,t,**kwargs):
-        self.force_val = force_val
-        self.t_start = t_start
-        self.t_end = t_end
-        self.force = 0.0
+# Define velocity inlet parabolic profile
+class VelInPara(UserExpression):
+    def __init__(self, t, vel_t_ramp, u_max, n, dsi, mesh, **kwargs):
         self.t = t
-
+        self.t_ramp = vel_t_ramp
+        self.u_max = u_max
+        self.n = n # normal direction
+        self.dsi = dsi # surface integral element
+        self.d = mesh.geometry().dim()
+        self.x = SpatialCoordinate(mesh)
+        # Compute area of boundary tessellation by integrating 1.0 over all facets
+        self.H = assemble(Constant(1.0, name="one")*self.dsi)
+        # Compute barycentre by integrating x components over all facets
+        self.c = [assemble(self.x[i]*self.dsi) / self.H for i in range(self.d)]
+        # Compute radius by taking max radius of boundary points
+        self.r = self.H / 2
         super().__init__(**kwargs)
 
     def update(self, t):
         self.t = t
-
-    def eval(self, value,x):
-        if self.t >= self.t_start and self.t<=self.t_end:
-            self.force = self.force_val 
+    
+    def eval(self, value, x):
+        #Define the velocity ramp
+        if self.t < self.t_ramp:
+            interp_PA = self.u_max*(-0.5*np.cos((pi/(self.t_ramp))*(self.t)) + 0.5)   # Velocity initialisation with sigmoid
         else:
-            self.force = 0.0
-        value[0] = self.force
-        value[1] = 0
+            interp_PA = self.u_max
+
+        # Define the parabola
+        r2 = (x[0]-self.c[0])**2 + (x[1]-self.c[1])**2  # radius**2
+        fact_r = 1 - (r2/self.r**2)
+        value[0] = -self.n[0] * (interp_PA) *fact_r  # *self.t # x-values
+        value[1] = -self.n[1] * (interp_PA) *fact_r  # *self.t # y-values
+
 
     def value_shape(self):
         return (2,)
+
+
+class ParabolicPressure(UserExpression):
+    def __init__(self, t, t_pressure_ramp, p_max, dsi, mesh, **kwargs):
+        super().__init__(**kwargs)
+        self.t = t
+        self.t_ramp = t_pressure_ramp
+        self.p_max = p_max
+        self.dsi = dsi # surface integral element
+        self.d = mesh.geometry().dim()
+        self.x = SpatialCoordinate(mesh)
+        # Compute area of boundary tessellation by integrating 1.0 over all facets
+        self.H = assemble(Constant(1.0, name="one")*self.dsi)
+        # Compute barycentre by integrating x components over all facets
+        self.c = [assemble(self.x[i]*self.dsi) / self.H for i in range(self.d)]
+        # Compute radius by taking max radius of boundary points
+        self.r = self.H / 2
+
+    def update(self, t):
+        self.t = t
+    
+    def eval(self, value, x):
+        #Define the pressure ramp
+        if self.t < self.t_ramp:
+            interp_PA = self.p_max*(-0.5*np.cos((pi/(self.t_ramp))*(self.t)) + 0.5)
+        else:
+            interp_PA = self.p_max
+
+        # Define the parabola
+        r2 = (x[0]-self.c[0])**2 + (x[1]-self.c[1])**2  # radius**2
+        fact_r = 1 - (r2/self.r**2)
+        value[0] = interp_PA *fact_r # pressure values 
+
+
+    def value_shape(self):
+        return ()
+
+
+# class PressureImpulse(UserExpression):
+#     def __init__(self, force_val, t_start,t_end,t,**kwargs):
+#         self.force_val = force_val
+#         self.t_start = t_start
+#         self.t_end = t_end
+#         self.force = 0.0
+#         self.t = t
+
+#         super().__init__(**kwargs)
+
+#     def update(self, t):
+#         self.t = t
+
+#     def eval(self, value,x):
+#         if self.t >= self.t_start and self.t<=self.t_end:
+#             self.force = self.force_val 
+#         else:
+#             self.force = 0.0
+#         value[0] = self.force
+#         value[1] = 0
+
+#     def value_shape(self):
+#         return (2,)
     
 # def initiate(dvp_, DVP, mesh, boundaries, **namespace):
 #     # get the dofs on the fluid inlet boundary
@@ -110,25 +185,50 @@ class PressureImpulse(UserExpression):
 #     # return dict(dvp_=dvp_)
 #     pass
 
-def create_bcs(DVP, boundaries, F_fluid_linear, psi, mesh, **namespace):
+def create_bcs(DVP, boundaries, v_deg, p_deg, mesh, **namespace):
 
     if MPI.rank(MPI.comm_world) == 0:
         print("Create bcs")
 
     # Solid displacement BCs / x-component of displacement at solid inlet and outlet is 0
-    d_s_inlet = DirichletBC(DVP.sub(0).sub(0), Constant(0), boundaries, 4)
-    d_s_outlet = DirichletBC(DVP.sub(0).sub(0), Constant(0), boundaries, 5)
-
+    # d_s_inlet = DirichletBC(DVP.sub(0).sub(0), Constant(0), boundaries, 4)
+    # d_s_outlet = DirichletBC(DVP.sub(0).sub(0), Constant(0), boundaries, 5)
+    d_s_inlet = DirichletBC(DVP.sub(0), ((0.0, 0.0)), boundaries, 4)
+    d_s_outlet = DirichletBC(DVP.sub(0), ((0.0, 0.0)), boundaries, 5)
     
+    # Solid velocity BCs / x-component of velocity at solid inlet and outlet is 0
+    # v_s_inlet = DirichletBC(DVP.sub(1).sub(0), Constant(0), boundaries, 4)
+    # v_s_outlet = DirichletBC(DVP.sub(1).sub(0), Constant(0), boundaries, 5)
+    v_s_inlet = DirichletBC(DVP.sub(1), ((0.0, 0.0)), boundaries, 4)
+    v_s_outlet = DirichletBC(DVP.sub(1), ((0.0, 0.0)), boundaries, 5)
 
     # Fluid displacement BCs / displacement at fluid inlet and outlet is 0 
     d_f_inlet = DirichletBC(DVP.sub(0), ((0.0, 0.0)), boundaries, 1)
     d_f_outlet = DirichletBC(DVP.sub(0), ((0.0, 0.0)), boundaries, 2)
 
-    # Fluid velocity BCs / no-slip at solid walls
-    inflow_profile = ('4*1.5*x[1]*(0.41 - x[1]) / pow(0.41, 2)', '0')
-    u_f_inlet = DirichletBC(DVP.sub(1), Expression(inflow_profile, degree=2), boundaries, 1)
+    # Fluid velocity BCs
+    dsi = ds(1, domain=mesh, subdomain_data=boundaries)
+    n = FacetNormal(mesh)
+    ndim = mesh.geometry().dim()
+    ni = np.array([assemble(n[i]*dsi) for i in range(ndim)])
+    n_len = np.sqrt(sum([ni[i]**2 for i in range(ndim)]))  
+    normal = ni/n_len
+    # Parabolic profile
+    vel_t_ramp = 0.2
+    u_max = 0.75
+    u_inflow_exp = VelInPara(t=0.0, vel_t_ramp=vel_t_ramp, u_max=u_max, n=normal, dsi=dsi, mesh=mesh, degree=v_deg)
+    # inflow_profile = ('4*1.5*x[1]*(0.41 - x[1]) / pow(0.41, 2)', '0')
+
+    # Fluid velocity BCs / velocity at fluid inlet is parabolic profile
+    u_f_inlet = DirichletBC(DVP.sub(1), u_inflow_exp, boundaries, 1)
+    # No slip BCs for fluid velocity at solid walls
     u_f_walls = DirichletBC(DVP.sub(1),  ((0.0, 0.0)), boundaries, 3)
+
+    # Fluid pressure BCs / pressure at fluid outlet is parabolic profile
+    dsi = ds(2, domain=mesh, subdomain_data=boundaries)
+    p_outlet_exp = ParabolicPressure(t=0.0, t_pressure_ramp=0.2, p_max=1, dsi=dsi, mesh=mesh, degree=p_deg)
+    p_f_outlet = DirichletBC(DVP.sub(2), p_outlet_exp, boundaries, 2)
+
     # ds for fluid inlet 
     # ds = Measure("ds", domain=mesh, subdomain_data=boundaries)
     # impulse_force = PressureImpulse(force_val=50, t_start=0.005, t_end=0.02, t=0.0)
@@ -141,13 +241,19 @@ def create_bcs(DVP, boundaries, F_fluid_linear, psi, mesh, **namespace):
     # p_n.vector()[bdry_dofs] = 5e3
     # bcp = DirichletBC(DVP.sub(2), p_n, boundaries, 1)
     # bcp_wall = DirichletBC(DVP.sub(2), Constant(0), boundaries, 3)
-    bcp_out = DirichletBC(DVP.sub(2), Constant(0), boundaries, 2)
+    # bcp_out = DirichletBC(DVP.sub(2), Constant(0), boundaries, 2)
     # Assemble boundary conditions
-    bcs = [d_s_inlet, d_s_outlet, d_f_inlet, d_f_outlet, u_f_walls, u_f_inlet, bcp_out]
+    bcs = [d_s_inlet, d_s_outlet, v_s_inlet, v_s_outlet, d_f_inlet, d_f_outlet, u_f_walls, u_f_inlet, p_f_outlet]
 
     # return dict(bcs=bcs, F_fluid_linear=F_fluid_linear, impulse_force=impulse_force)
-    return dict(bcs=bcs)
+    return dict(bcs=bcs, u_inflow_exp=u_inflow_exp, p_outlet_exp=p_outlet_exp)
 
+
+def pre_solve(t, u_inflow_exp, p_outlet_exp, **namespace):
+    # Update the time variable used for the inlet boundary condition
+    u_inflow_exp.update(t)
+    p_outlet_exp.update(t)
+    return dict(u_inflow_exp=u_inflow_exp, p_outlet_exp=p_outlet_exp)
 
 # def pre_solve(t, impulse_force, **namespace):
 #     # Update the time variable used for the inlet boundary condition
