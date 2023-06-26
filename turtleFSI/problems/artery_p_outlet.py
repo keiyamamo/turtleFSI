59658bc0d32fef027b1385b1a90101bac5afc7dc
Kei <withwhom0215@gmail.com>
2023-06-26 13:25:54 +0200

    added problem file with pressure outlet term

---
+++ turtleFSI/problems/artery_p_outlet.py
@@ -0,0 +1,211 @@
+from os import path
+import numpy as np
+from numpy import genfromtxt
+
+from dolfin import *
+from turtleFSI.problems import *
+from utils.Womersley import make_womersley_bcs, compute_boundary_geometry_acrn
+from turtleFSI.modules.common import J_, F_
+
+# set compiler arguments
+parameters["form_compiler"]["quadrature_degree"] = 6
+parameters["form_compiler"]["optimize"] = True
+# The "ghost_mode" has to do with the assembly of form containing the facet
+# normals n('+') within interior boundaries (dS). for 3D mesh the value should
+# be "shared_vertex", for 2D mesh "shared_facet", the default value is "none"
+parameters["ghost_mode"] = "shared_vertex"
+_compiler_parameters = dict(parameters["form_compiler"])
+
+
+def set_problem_parameters(default_variables, **namespace):
+    
+    # Read problem-specific variables from config file
+    x_sphere = 0.123043
+    y_sphere = 0.13458
+    z_sphere = 0.064187
+    r_sphere = 0.004
+    dt = 0.00033964286
+    mesh_path = "file_case9_el047"
+    save_deg_sim = 2
+    Q_mean = 1.9275E-06
+
+    # Overwrite default values
+    E_s_val = 1E6
+    nu_s_val = 0.45
+    mu_s_val = E_s_val/(2*(1+nu_s_val))  # 0.345E6
+    lambda_s_val = nu_s_val*2.*mu_s_val/(1. - 2.*nu_s_val)
+
+    default_variables.update(dict(
+        T=0.3,     # Simulation end time
+        dt=dt,#0.00033964286, # Timne step size
+        atol=1e-6, # Absolute tolerance in the Newton solver
+        rtol=1e-6,# Relative tolerance in the Newton solver
+        inlet_id=2,  # inlet
+        inlet_outlet_s_id=11,  # also the "rigid wall" id for the stucture problem
+        recompute=5, # Recompute the Jacobian matix within time steps                                                                                    
+        recompute_tstep=2, # Recompute the Jacobian matix over time steps (dangerous!)  
+        fsi_id=22,  # fsi surface
+        rigid_id=11,  # "rigid wall" id for the fluid and mesh problem
+        ds_s_id=[33],                     # ID of solid external wall (where we want to test Robin BC)
+        outer_id=33,  # outer surface
+        folder=mesh_path,
+        mesh_file=mesh_path,
+        Q_file="MCA_10", # This is the location of CFD results used to prescribe the inlet velocity profile
+        Q_mean=Q_mean,#1.9275E-06, # Problem specific
+        theta=0.501, # Theta scheme (implicit/explicit time stepping)
+        rho_f=1.000E3,    # Fluid density [kg/m3]
+        mu_f=3.5E-3,       # Fluid dynamic viscosity [Pa.s]
+        rho_s=1.0E3,    # Solid density [kg/m3]
+        mu_s=mu_s_val,     # Solid shear modulus or 2nd Lame Coef. [Pa]
+        nu_s=nu_s_val,      # Solid Poisson ratio [-]
+        lambda_s=lambda_s_val,  # Solid Young's modulus [Pa]
+        dx_f_id=1,      # ID of marker in the fluid domain
+        dx_s_id=2,      # ID of marker in the solid domain
+        extrapolation="laplace",  # laplace, elastic, biharmonic, no-extrapolation
+        extrapolation_sub_type="constant",  # ["constant", "small_constant", "volume", "volume_change", "bc1", "bc2"]
+        compiler_parameters=_compiler_parameters,  # Update the defaul values of the compiler arguments (FEniCS)
+        linear_solver="mumps",  # use list_linear_solvers() to check alternatives
+        checkpoint_step=50, # CHANGE
+        save_step=1, # Save frequency of files for visualisation
+        save_deg=save_deg_sim,          # Degree of the functions saved for visualisation '1' '2' '3' etc... (high value can slow down simulation significantly!)
+        fsi_region=[x_sphere,y_sphere,z_sphere,r_sphere], # X, Y, and Z coordinate of FSI region center, radius of spherical deformable region (outside this region the walls are rigid)
+        p_wave_file = "p_t.csv", # File containing the pressure wave
+    ))
+
+    return default_variables
+
+
+def get_mesh_domain_and_boundaries(mesh_file,fsi_region, fsi_id, rigid_id, outer_id, folder, **namespace):
+    # Read mesh
+    mesh = Mesh()
+    hdf = HDF5File(mesh.mpi_comm(), "mesh/" + mesh_file + ".h5", "r")
+    hdf.read(mesh, "/mesh", False)
+    boundaries = MeshFunction("size_t", mesh, 2)
+    hdf.read(boundaries, "/boundaries")
+    domains = MeshFunction("size_t", mesh, 3)
+    hdf.read(domains, "/domains")
+
+    # Only considere FSI in domain within this sphere BC1
+    sph_x = float(fsi_region[0])
+    sph_y = float(fsi_region[1])
+    sph_z = float(fsi_region[2])
+    sph_rad = float(fsi_region[3])
+
+    i = 0
+    # Fluid-solid interface is by defualt assumed to have fsi id. Therefore, we compute the distance from the midpoint of the cell
+    # to the center of the sphere and then change id from fsi to rigid wall if the distance is larger than the radius of the sphere
+    for submesh_facet in facets(mesh):
+        idx_facet = boundaries.array()[i]
+        if idx_facet == fsi_id or idx_facet == outer_id:
+            mid = submesh_facet.midpoint()
+            dist_sph_center = sqrt((mid.x()-sph_x)**2 + (mid.y()-sph_y)**2 + (mid.z()-sph_z)**2)
+            if dist_sph_center > sph_rad:
+                boundaries.array()[i] = rigid_id  # changed "fsi" idx to "rigid wall" idx
+        i += 1
+
+    # f = File(f'{mesh_file}.pvd')
+    # f << boundaries
+    # f << domains
+    # exit(1)
+
+    return mesh, domains, boundaries
+
+
+class InnerP(UserExpression):
+    def __init__(self, t, t_ramp, t_p, p_PA, **kwargs):
+        self.t = t
+        self.t_ramp = t_ramp
+        self.t_p = t_p
+        self.p_PA = p_PA    
+        self.p_0 = 0.0 # Initial pressure
+        self.P = self.p_0 # Apply initial pressure to inner pressure variable
+        super().__init__(**kwargs)
+
+    def update(self, t):
+        self.t = t
+        # apply a sigmoid ramp to the pressure 
+        if self.t < self.t_ramp:
+            ramp_factor = (-1/2)*np.cos(3.14159*self.t/self.t_ramp) + 1/2
+        else:
+            ramp_factor = 1.0
+        if MPI.rank(MPI.comm_world) == 0:
+            print("ramp_factor = {} m^3/s".format(ramp_factor))
+        # Caclulate P as resistance boundary condition multiplied by ramp factor
+        self.P = ramp_factor * (np.interp(self.t, self.t_p, self.p_PA))
+        if MPI.rank(MPI.comm_world) == 0:
+            print("P = {} Pa".format(self.P))
+
+    def eval(self, value, x):
+        value[0] = self.P 
+
+    def value_shape(self):
+        return ()
+
+
+def create_bcs(t, DVP, mesh, boundaries, mu_f, dvp_,
+               inlet_id, inlet_outlet_s_id,
+               rigid_id, psi, F_fluid_nonlinear, p_deg, Q_file, Q_mean, p_wave_file, **namespace):
+
+    # Load normalized time and flow rate values
+    t_values, Q_ = np.loadtxt(path.join(path.dirname(path.abspath(__file__)), Q_file)).T
+    Q_values = Q_mean * Q_  # Specific flow rate = Normalized flow wave form * Prescribed flow rate
+    tmp_area, tmp_center, tmp_radius, tmp_normal = compute_boundary_geometry_acrn(mesh, inlet_id, boundaries)
+
+    # Create Womersley boundary condition at inlet
+    inlet = make_womersley_bcs(t_values, Q_values, mesh, mu_f, tmp_area, tmp_center, tmp_radius, tmp_normal, DVP.sub(1).sub(0).ufl_element())
+
+    # Initialize inlet expressions with initial time
+    for uc in inlet:
+        uc.set_t(t)
+
+    # Create Boundary conditions for the velocity
+    u_inlet = [DirichletBC(DVP.sub(1).sub(i), inlet[i], boundaries, inlet_id) for i in range(3)]
+    u_inlet_s = DirichletBC(DVP.sub(1), ((0.0, 0.0, 0.0)), boundaries, inlet_outlet_s_id)
+
+    # Solid Displacement BCs
+    d_inlet = DirichletBC(DVP.sub(0), (0.0, 0.0, 0.0), boundaries, inlet_id)
+    d_inlet_s = DirichletBC(DVP.sub(0), (0.0, 0.0, 0.0), boundaries, inlet_outlet_s_id)
+    d_rigid = DirichletBC(DVP.sub(0), (0.0, 0.0, 0.0), boundaries, rigid_id)
+
+    # Assemble boundary conditions
+    bcs = u_inlet + [d_inlet, u_inlet_s, d_inlet_s, d_rigid]
+
+    # Define the pressure condition (apply to inner surface, numerical instability results from applying to outlet)
+    p_t_file = genfromtxt(p_wave_file, delimiter=',')
+    t_pressure=p_t_file[1:,0]
+    pressure_PA=p_t_file[1:,1]
+    p_out_bc_val = InnerP(t=0.0, t_p=t_pressure, t_ramp=0.2, p_PA=pressure_PA, degree=p_deg)
+
+    # Neumann BC for fluid outlet as traction / here we assume that the pressure is the dominant term in the stress tensor
+    # Two outlet surface has ID 3 and 4
+    dso_1 = ds(3, domain=mesh, subdomain_data=boundaries)
+    dso_2 = ds(4, domain=mesh, subdomain_data=boundaries)
+
+    n_1 = FacetNormal(dso_1)
+    n_2 = FacetNormal(dso_2)
+    pf = p_out_bc_val * Identity(3)
+    d = dvp_["n"].sub(0, deepcopy=True)
+    F_fluid_nonlinear += inner(J_(d) * pf * inv(F_(d)).T * n_1, psi) * dso_1
+    F_fluid_nonlinear += inner(J_(d) * pf * inv(F_(d)).T * n_2, psi) * dso_2
+
+    return dict(bcs=bcs, inlet=inlet, p_out_bc_val=p_out_bc_val, F_fluid_nonlinear=F_fluid_nonlinear)
+
+
+def pre_solve(t, inlet, p_out_bc_val, **namespace):
+    for uc in inlet:
+        # Update the time variable used for the inlet boundary condition
+        uc.set_t(t)
+
+        # Multiply by cosine function to ramp up smoothly over time interval 0-250 ms
+        if t < 0.25:
+            uc.scale_value = -0.5 * np.cos(np.pi * t / 0.25) + 0.5
+        else:
+            uc.scale_value = 1.0
+
+    # Update pressure condition
+    p_out_bc_val.update(t)
+
+    return dict(inlet=inlet, p_out_bc_val=p_out_bc_val)
+
+def finished(**namespace):
+    with open("finished", mode='a'): pass
