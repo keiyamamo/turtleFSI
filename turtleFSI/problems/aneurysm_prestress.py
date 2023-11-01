d713695df5be1c52405399e024cb9470b84e2e39
Kei <withwhom0215@gmail.com>
2023-11-01 10:56:04 +0100

    update problem file for pre-stress simulation

--- turtleFSI/problems/aneurysm_prestress.py
+++ turtleFSI/problems/aneurysm_prestress.py
@@ -26,38 +26,48 @@ def set_problem_parameters(default_variables, **namespace):
     lambda_s_val = nu_s_val*2.*mu_s_val/(1. - 2.*nu_s_val)
 
     default_variables.update(dict(
+        # Time variables
         T=1.0, # Simulation end time
-        dt=0.000679286, # Timne step size
+        dt=0.001, # Timne step size
+        # Solver parameters
         atol=1e-5, # Absolute tolerance in the Newton solver
         rtol=1e-5,# Relative tolerance in the Newton solver
+        recompute=15, # Recompute the Jacobian matix within time steps                                                                                    
+        recompute_tstep=100, # Recompute the Jacobian matix over time steps (dangerous!)  
+        theta=0.501, # Theta scheme (implicit/explicit time stepping)
+        linear_solver="mumps",  # use list_linear_solvers() to check alternatives
+        # boundary conditions
+        mesh_file="file_case3",
         inlet_id=2,  # inlet
-        outlet_id3=3,  # outlet nb1
-        outlet_id4=4,  # outlet nb2
         inlet_outlet_s_id=11,  # also the "rigid wall" id for the stucture problem
-        recompute=60, # Recompute the Jacobian matix within time steps                                                                                    
-        recompute_tstep=10, # Recompute the Jacobian matix over time steps (dangerous!)  
         fsi_id=22,  # fsi surface
         rigid_id=11,  # "rigid wall" id for the fluid and mesh problem
         outer_id=33,  # outer surface
-        folder="file_case3",
-        mesh_file="file_case3",
-        theta=0.501, # Theta scheme (implicit/explicit time stepping)
+        dx_f_id=1,      # ID of marker in the fluid domain
+        dx_s_id=2,      # ID of marker in the solid domain
+        fsi_region=[x_sphere,y_sphere,z_sphere,r_sphere], # X, Y, and Z coordinate of FSI region center, radius of spherical deformable region (outside this region the walls are rigid)
+        # Fluid parameters
         rho_f=1.000E3,    # Fluid density [kg/m3]
         mu_f=3.5E-3,       # Fluid dynamic viscosity [Pa.s]
+        u_max = 0.74, # Maximum velocity of the inlet wheres the average velocity is 0.37
+        # Pressure parameters
+        p_val = 10932.404, # 82mmHg in Pa
+        p_t_ramp_start = 0.2,
+        p_t_ramp_end = 0.4,
+        # Solid parameters
         rho_s=1.0E3,    # Solid density [kg/m3]
         mu_s=mu_s_val,     # Solid shear modulus or 2nd Lame Coef. [Pa]
         nu_s=nu_s_val,      # Solid Poisson ratio [-]
         lambda_s=lambda_s_val,  # Solid Young's modulus [Pa]
-        dx_f_id=1,      # ID of marker in the fluid domain
-        dx_s_id=2,      # ID of marker in the solid domain
+        # Mesh lifting parameters
         extrapolation="laplace",  # laplace, elastic, biharmonic, no-extrapolation
         extrapolation_sub_type="constant",  # ["constant", "small_constant", "volume", "volume_change", "bc1", "bc2"]
-        compiler_parameters=_compiler_parameters,  # Update the defaul values of the compiler arguments (FEniCS)
-        linear_solver="mumps",  # use list_linear_solvers() to check alternatives
-        fsi_region=[x_sphere,y_sphere,z_sphere,r_sphere], # X, Y, and Z coordinate of FSI region center, radius of spherical deformable region (outside this region the walls are rigid)
-        checkpoint_step=100, # 
-        save_step=1, # Save frequency of files for visualisation
+        # I/O parameters
+        save_step=50, # Save frequency of files for visualisation
         save_deg=1,          # Degree of the functions saved for visualisation '1' '2' '3' etc... (high value can slow down simulation significantly!)
+        checkpoint_step=100, # 
+        compiler_parameters=_compiler_parameters,  # Update the defaul values of the compiler arguments (FEniCS)
+        folder="file_case3",
     ))
 
     return default_variables
@@ -102,58 +112,11 @@ def get_mesh_domain_and_boundaries(mesh_file, fsi_id, rigid_id, outer_id, fsi_re
     return mesh, domains, boundaries
 
 
-class StokesPrb():
-
-    def __init__(self, mesh, domains, boundaries, dsi, inlet_id, outlet_id3, outlet_id4, normal, mu_f):
-
-        self.mesh = mesh
-        self.ve = VectorElement('CG', self.mesh.ufl_cell(), 2)
-        self.pe = FiniteElement('CG', self.mesh.ufl_cell(), 1)
-        self.Elem = MixedElement([self.ve, self.pe])
-        self.VP = FunctionSpace(self.mesh, self.Elem)
-
-        self.u_inflow_exp = VelInPara(t=0.0, t_ramp=0.0, n=normal, dsi=dsi, mesh=mesh, degree=3)
-        self.u_inlet = DirichletBC(self.VP.sub(0), self.u_inflow_exp, boundaries, inlet_id)
-        self.u_fsi1 = DirichletBC(self.VP.sub(0), ((0.0, 0.0, 0.0)), boundaries, 11)
-        self.u_fsi2 = DirichletBC(self.VP.sub(0), ((0.0, 0.0, 0.0)), boundaries, 22)
-        self.u_fsi3 = DirichletBC(self.VP.sub(0), ((0.0, 0.0, 0.0)), boundaries, 33)
-
-        self.Pout_val = Constant(0.0)
-
-        self.bcs = [self.u_inlet, self.u_fsi1, self.u_fsi2, self.u_fsi3]
-
-        self.ds = Measure("ds", domain=self.mesh, subdomain_data=boundaries)
-        self.n = FacetNormal(self.mesh)
-        # Define variational problem
-        (self.u, self.p) = TrialFunctions(self.VP)
-        (self.v, self.q) = TestFunctions(self.VP)
-        f = Constant((0.0, 0.0, 0.0))
-        self.a = (mu_f*inner(grad(self.u), grad(self.v))*dx
-                  - self.p*div(self.v)*dx
-                  + self.q*div(self.u)*dx)
-        self.L = (inner(f, self.v)*dx
-                  - self.Pout_val * inner(self.n, self.v)*self.ds(outlet_id4)
-                  - self.Pout_val * inner(self.n, self.v)*self.ds(outlet_id3))
-
-    def solve(self):
-
-        # Solution vector
-        self.w = Function(self.VP)
-        # Assemble system and Solve
-        A, B = assemble_system(self.a, self.L, self.bcs)
-        #solve(A, self.w.vector(), B, "minres", "amg")
-        loc_sol = LUSolver(Matrix(), "mumps")
-        loc_sol.solve(A, self.w.vector(), B)
-        # Get sub-functions
-        self.u, self.p = self.w.split(deepcopy=True)
-
-        return self.u, self.p
-
-
 class VelInPara(UserExpression):
-    def __init__(self, t, t_ramp, n, dsi, mesh, **kwargs):
+    def __init__(self, t, t_ramp, u_max, n, dsi, mesh, **kwargs):
         self.t = t
-        self.t1 = t_ramp
+        self.t_ramp = t_ramp
+        self.u_max = u_max
         self.n = n
         self.dsi = dsi
         self.d = mesh.geometry().dim()
@@ -170,8 +133,10 @@ class VelInPara(UserExpression):
         self.t = t
         
     def eval(self, value, x):
-        # seems like doulbe of 0.37 m/s
-        interp_PA = 0.74
+        if self.t < self.t_ramp:
+            interp_PA = self.u_max*(-0.5*np.cos((pi/(self.t_ramp))*(self.t)) + 0.5)  # Velocity initialisation with sigmoid
+        else:
+            interp_PA = self.u_max
 
         r2 = (x[0]-self.c[0])**2 + (x[1]-self.c[1])**2 + (x[2]-self.c[2])**2  # radius**2
         fact_r = 1 - (r2/self.r**2)
@@ -184,32 +149,37 @@ class VelInPara(UserExpression):
         return (3,)
 
 
+# Define the pressure ramp
 class InnerP(UserExpression):
-    def __init__(self, t, **kwargs):
-        self.t = t     
+    def __init__(self, t, p_val, p_t_ramp_start, p_t_ramp_end, **kwargs):
+        self.t = t 
+        self.p_val = p_val
+        self.p_t_ramp_start = p_t_ramp_start
+        self.p_t_ramp_end = p_t_ramp_end
         super().__init__(**kwargs)
 
     def eval(self, value, x):
-        
-        if self.t < 0.5:
-            value[0] = 10932.404*np.sin(3.14159*self.t) # sine that has same slope as the end
+        if self.t < self.p_t_ramp_start:
+            value[0] = 0.0
+        elif self.t < self.p_t_ramp_end:
+            value[0] = self.p_val*(-0.5*np.cos((pi/(self.p_t_ramp_end - self.p_t_ramp_start))*(self.t - self.p_t_ramp_start)) + 0.5) # Pressure initialisation with sigmoid
         else:
-            value[0] = 10932.404 
+            value[0] = self.p_val
 
     def value_shape(self):
         return ()
 
 
-def create_bcs(dvp_, DVP, mesh, boundaries, domains, mu_f,
-               fsi_id, outlet_id3, outlet_id4, inlet_id, inlet_outlet_s_id,
-               rigid_id, psi, F_solid_linear, **namespace):
+def create_bcs(dvp_, DVP, mesh, boundaries, p_val, p_t_ramp_start, p_t_ramp_end, p_deg,
+               fsi_id, inlet_id, inlet_outlet_s_id,
+               rigid_id, psi, F_solid_linear, u_max, **namespace):
 
     if MPI.rank(MPI.comm_world) == 0:
         print("Create bcs")
 
     # Define the pressure condition necessary to create the variational form (Neumann BCs)
 
-    p_out_bc_val = InnerP(t=0.0, degree=2)
+    p_out_bc_val = InnerP(t=0.0, p_val=p_val, p_t_ramp_start=p_t_ramp_start, p_t_ramp_end=p_t_ramp_end, degree=p_deg)
 
     dSS = Measure("dS", domain=mesh, subdomain_data=boundaries)
     n = FacetNormal(mesh)
@@ -224,7 +194,7 @@ def create_bcs(dvp_, DVP, mesh, boundaries, domains, mu_f,
     normal = ni/n_len
 
     # new Parabolic profile
-    u_inflow_exp = VelInPara(t=0.0, t_ramp=0.0, n=normal, dsi=dsi, mesh=mesh, degree=3)
+    u_inflow_exp = VelInPara(t=0.0, t_ramp=0.2, u_max=u_max, n=normal, dsi=dsi, mesh=mesh, degree=2)
     u_inlet = DirichletBC(DVP.sub(1), u_inflow_exp, boundaries, inlet_id)
     u_inlet_s = DirichletBC(DVP.sub(1), ((0.0, 0.0, 0.0)), boundaries, inlet_outlet_s_id)
 
@@ -235,16 +205,6 @@ def create_bcs(dvp_, DVP, mesh, boundaries, domains, mu_f,
 
     # Assemble boundary conditions
     bcs = [u_inlet, d_inlet,  u_inlet_s, d_inlet_s, d_rigid]
-
-    # Solve Stokes problem
-    Stokes = StokesPrb(mesh, domains, boundaries, dsi, inlet_id, outlet_id3, outlet_id4, normal, mu_f)
-    sol = Stokes.solve()
-    assign(dvp_["n"].sub(1), sol[0])
-    assign(dvp_["n"].sub(2), sol[1])
-    assign(dvp_["n-1"].sub(1), sol[0])
-    assign(dvp_["n-1"].sub(2), sol[1])
-    assign(dvp_["n-2"].sub(1), sol[0])
-    assign(dvp_["n-2"].sub(2), sol[1])
 
     return dict(bcs=bcs, u_inflow_exp=u_inflow_exp, p_out_bc_val=p_out_bc_val,
                 F_solid_linear=F_solid_linear)
